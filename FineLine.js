a0959eddab43607800dabf7017b16ed58617d934
Maria10T <maria_t1006@yahoo.com>
2022-05-19 11:37:34 +0300

    Edit music

--- FineLine.js
+++ FineLine.js
@@ -7,9 +7,7 @@ function play(id) {
 
   activeSong.play();
 
-  var percentageOfVolume = activeSong.volume / 1;
-  var percentageOfVolumeMeter = document.getElementById('volumeMeter').offsetWidth * percentageOfVolume;
-
+  
 
   document.getElementById('volumeStatus').style.width = Math.round(percentageOfVolumeSlider) + "px";
 }
@@ -51,7 +49,7 @@ function updateTime() {
 
 
   document.getElementById('trackProgress').style.width = Math.round(percentageOfSlider) + "px";
-  document.getElementById('trackProgress').style.backgroundColor = "red";
+
 }
 
 
